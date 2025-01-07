b708efa0fab205e8048ab8dd137b43103e54b14f
Vitaly Trakhtenberg <webdev@tca.by>
2025-01-07 13:41:02 +0200

    getCount() - updated to increase effectivity

--- src/codewars/vowelCount.php
+++ src/codewars/vowelCount.php
@@ -1,7 +1,13 @@
 <?php
 function getCount(string $str): int {
-    preg_match_all('/[aeiou]/', $str, $out);
-    return count($out[0]);
+    /*preg_match_all('/[aeiou]/', $str, $out);
+    return count($out[0]);*/
+
+    /*
+     * Использование модификатора i: Этот модификатор делает поиск нечувствительным к регистру, что позволяет находить как строчные, так и заглавные гласные без дополнительных усилий.
+     * Возврат значения функцией: preg_match_all напрямую возвращает количество найденных соответствий, что делает код очень кратким.
+     */
+    return preg_match_all('/[aeiou]/i', $str, $matches);
 }
 
 echo getCount("aeiou"); echo PHP_EOL;
