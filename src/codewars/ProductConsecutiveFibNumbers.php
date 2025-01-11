5fadbd643b96a4a6161f3917af241cafc8b6bde1
Vitaly Trakhtenberg <webdev@tca.by>
2025-01-11 19:22:39 +0200

    productFib() - added new partially solved task. Question about one test case on discord now

---
+++ src/codewars/ProductConsecutiveFibNumbers.php
@@ -0,0 +1,51 @@
+<?php
+function isPerfectSquare($x): bool
+{
+    $s = (int)sqrt($x);
+    return $s * $s === $x;
+}
+
+function isFibonacci($n)
+{
+    // Следствие из формулы Бине - является ли 5*n^2 + 4 или 5*n^2 - 4 полным квадратом
+    return isPerfectSquare(5 * $n * $n + 4) || isPerfectSquare(5 * $n * $n - 4);
+}
+
+function findDivisors($n)
+{
+    $divisors = [];
+    for ($i = 2; $i * $i <= $n; $i++) {
+        if ($n % $i === 0) {
+            $pair = [$i, $n / $i];
+            if ($pair[0] < $pair[1]) {
+                $divisors[] = $pair;
+            }
+        }
+    }
+    return $divisors;
+}
+
+function productFib($prod)
+{
+    $result = [0, 0, false];
+    for ($i = 2; $i * $i <= $prod; $i++) {
+        if ($prod % $i === 0) {
+            $pair = [$i, $prod / $i];
+            if ($pair[0] < $pair[1]) {
+                if (isFibonacci($pair[0]) && isFibonacci($pair[1])) {
+                    $result = [$pair[0], $pair[1], true];
+                    break;
+                } else {
+                    $result = [$pair[0], $pair[1], false];
+                }
+            }
+        }
+    }
+    return $result;
+}
+
+var_dump(productFib(714)); echo PHP_EOL; // test passed
+var_dump(productFib(800)); echo PHP_EOL; // test passed
+var_dump(productFib(4895)); echo PHP_EOL; // test passed
+var_dump(productFib(5895)); echo PHP_EOL;   // test failed - result should be [89, 144, false]????
+var_dump(productFib(74049690)); echo PHP_EOL; // test passed
